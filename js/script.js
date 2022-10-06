90fc45b2651a95afb91ea40964af748a0cc034e5
Juan-González29 <juan.gonzalez9@udea.edu.co>
2022-10-06 00:13:03 -0500

    datos Booleans, operadores de Comparación y lógicos

--- js/script.js
+++ js/script.js
@@ -33,3 +33,92 @@ return 5 + numUno + numDos;
     var numUno = "120";
     var numDos = 10;
     console.log(numUno - numDos);
+
+
+var day = "Lunes";
+    if (day === "Miercoles") {
+        console.log("Hoy se lee metafísica");
+    } else {
+        console.log("Hay que trabajar y estudiar.");
+    }   
+
+var day = "Miercoles";
+    if (day === "Miercoles") {
+        console.log("Hoy se lee metafísica");
+    } else {
+        console.log("Hay que trabajar y estudiar.");
+    } 
+
+function estrictamenteDif (arg1, arg2) {
+return arg1 !== arg2;
+}
+    console.log(estrictamenteDif(4, "4"));
+
+
+function distintoA (arg1, arg2) {
+return arg1 != arg2;
+ }
+    console.log(distintoA(4, "4"));
+
+    console.log("hola" === "HOLA");
+
+
+var num = 5;
+    if (num !== 6) {
+        console.log("estrictamente distinto")
+    } else {
+        console.log("No se cumple la regla")
+    }
+
+
+var edad = 50;
+
+var edadEsPar = ((edad % 2) === 0);
+    console.log(edadEsPar)
+
+
+var edad = 25;
+
+var edadEsPar = ((edad % 2) === 0);
+        console.log(edadEsPar)
+
+
+var anioNacimiento = 1989;
+    if (anioNacimiento >= 1980 && anioNacimiento <= 1995) {
+        console.log("Generación Milenial");
+    }
+
+
+ //var pais = "Ecuador";
+   // if (pais === "EEUU" || pais === "Canadá" || pais === "México") {
+     //   alert("Tu país está en América del Norte");
+    //} else {
+     //   alert("Tu país NO está en América del Norte")
+    //}
+
+
+function esPar (unNumero) {
+    if (unNumero % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+        console.log(esPar(6));
+        console.log(esPar(32));
+        console.log(esPar(71));
+
+
+function mayorNumero (numUno, numDos) {
+    if (numUno > numDos) {
+        return numUno;
+    } else if (numUno === numDos) {
+        return "Los números son iguales";
+    } else {
+        return numDos;
+    }
+}
+
+        console.log(mayorNumero(13, 26));
+        console.log(mayorNumero(43, 17));
+        console.log(mayorNumero(5, 5));
